e6ba735844d67056595a225f9436a7a77b77fc77
Hardikkumar <hardikkumar@hardikkumarsmbp.localdomain>
2019-10-11 15:59:35 +0200

    update test data for api

--- api_modules/test_data/resources.js
+++ api_modules/test_data/resources.js
@@ -1,83 +1,85 @@
+import { vehicleId } from '../constant';
+
 // response body for /resources api
 export const vehical_resources = [
     {
-      "name": "decklidstatus",
-      "version": "1.0",
-      "href": "/vehicles/WDB111111ZZZ22222/resources/decklidstatus"
+      'name': 'decklidstatus',
+      'version': '1.0',
+      'href': `/vehicles/${vehicleId}/resources/decklidstatus`
     },
     {
-      "name": "doorstatusfrontleft",
-      "version": "1.0",
-      "href": "/vehicles/WDB111111ZZZ22222/resources/doorstatusfrontleft"
+      'name': 'doorstatusfrontleft',
+      'version': '1.0',
+      'href': `/vehicles/${vehicleId}/resources/doorstatusfrontleft`
     },
     {
-      "name": "doorstatusfrontright",
-      "version": "1.0",
-      "href": "/vehicles/WDB111111ZZZ22222/resources/doorstatusfrontright"
+      'name': 'doorstatusfrontright',
+      'version': '1.0',
+      'href': `/vehicles/${vehicleId}/resources/doorstatusfrontright`
     },
     {
-      "name": "doorstatusrearleft",
-      "version": "1.0",
-      "href": "/vehicles/WDB111111ZZZ22222/resources/doorstatusrearleft"
+      'name': 'doorstatusrearleft',
+      'version': '1.0',
+      'href': `/vehicles/${vehicleId}/resources/doorstatusrearleft`
     },
     {
-      "name": "doorstatusrearright",
-      "version": "1.0",
-      "href": "/vehicles/WDB111111ZZZ22222/resources/doorstatusrearright"
+      'name': 'doorstatusrearright',
+      'version': '1.0',
+      'href': `/vehicles/${vehicleId}/resources/doorstatusrearright`
     },
     {
-      "name": "interiorLightsFront",
-      "version": "1.0",
-      "href": "/vehicles/WDB111111ZZZ22222/resources/interiorLightsFront"
+      'name': 'interiorLightsFront',
+      'version': '1.0',
+      'href': `/vehicles/${vehicleId}/resources/interiorLightsFront`
     },
     {
-      "name": "interiorLightsRear",
-      "version": "1.0",
-      "href": "/vehicles/WDB111111ZZZ22222/resources/interiorLightsRear"
+      'name': 'interiorLightsRear',
+      'version': '1.0',
+      'href': `/vehicles/${vehicleId}/resources/interiorLightsRear`
     },
     {
-      "name": "lightswitchposition",
-      "version": "1.0",
-      "href": "/vehicles/WDB111111ZZZ22222/resources/lightswitchposition"
+      'name': 'lightswitchposition',
+      'version': '1.0',
+      'href': `/vehicles/${vehicleId}/resources/lightswitchposition`
     },
     {
-      "name": "readingLampFrontLeft",
-      "version": "1.0",
-      "href": "/vehicles/WDB111111ZZZ22222/resources/readingLampFrontLeft"
+      'name': 'readingLampFrontLeft',
+      'version': '1.0',
+      'href': `/vehicles/${vehicleId}/resources/readingLampFrontLeft`
     },
     {
-      "name": "readingLampFrontRight",
-      "version": "1.0",
-      "href": "/vehicles/WDB111111ZZZ22222/resources/readingLampFrontRight"
+      'name': 'readingLampFrontRight',
+      'version': '1.0',
+      'href': `/vehicles/${vehicleId}/resources/readingLampFrontRight`
     },
     {
-      "name": "rooftopstatus",
-      "version": "1.0",
-      "href": "/vehicles/WDB111111ZZZ22222/resources/rooftopstatus"
+      'name': 'rooftopstatus',
+      'version': '1.0',
+      'href': `/vehicles/${vehicleId}/resources/rooftopstatus`
     },
     {
-      "name": "sunroofstatus",
-      "version": "1.0",
-      "href": "/vehicles/WDB111111ZZZ22222/resources/sunroofstatus"
+      'name': 'sunroofstatus',
+      'version': '1.0',
+      'href': `/vehicles/${vehicleId}/resources/sunroofstatus`
     },
     {
-      "name": "windowstatusfrontleft",
-      "version": "1.0",
-      "href": "/vehicles/WDB111111ZZZ22222/resources/windowstatusfrontleft"
+      'name': 'windowstatusfrontleft',
+      'version': '1.0',
+      'href': `/vehicles/${vehicleId}/resources/windowstatusfrontleft`
     },
     {
-      "name": "windowstatusfrontright",
-      "version": "1.0",
-      "href": "/vehicles/WDB111111ZZZ22222/resources/windowstatusfrontright"
+      'name': 'windowstatusfrontright',
+      'version': '1.0',
+      'href': `/vehicles/${vehicleId}/resources/windowstatusfrontright`
     },
     {
-      "name": "windowstatusrearleft",
-      "version": "1.0",
-      "href": "/vehicles/WDB111111ZZZ22222/resources/windowstatusrearleft"
+      'name': 'windowstatusrearleft',
+      'version': '1.0',
+      'href': `/vehicles/${vehicleId}/resources/windowstatusrearleft`
     },
     {
-      "name": "windowstatusrearright",
-      "version": "1.0",
-      "href": "/vehicles/WDB111111ZZZ22222/resources/windowstatusrearright"
+      'name': 'windowstatusrearright',
+      'version': '1.0',
+      'href': `/vehicles/${vehicleId}/resources/windowstatusrearright`
     }
   ];
